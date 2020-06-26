f18ca4583738550235014195819155f3a03766f1
Marcio Pancheri <mjpancheri@claretiano.edu.br>
2020-06-26 09:02:55 -0300

    Adicionando o projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
+import { Form } from '@unform/web';
+import { Scope } from '@unform/core';
+import * as Yup from 'yup';
 import './App.css';
 
+import Input from './components/Form/Input';
+//usar initialData={initialData} no form somente para inicializar com dados estaticos
+// const initialData = {
+//   email: 'mjpancheri@gmail.com',
+//   address: {
+//     city: 'Batatais',
+//   }
+// }
+
 function App() {
+  //const [user, setUser] = useState({});
+  const formRef = useRef(null);
+  async function handleSubmit(data, { reset }){
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .email('Digite um email válido')
+          .required('O email é obrigatório'),
+        address: Yup.object().shape({
+          city: Yup.string()
+            .min(3, 'No mínimo 3 caracteres')
+            .required('A cidade é obrigatória')
+        })
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      console.log(data);
+      formRef.current.setErrors({});
+      reset();
+    }catch(err){
+      if(err instanceof Yup.ValidationError){
+        const errorMessages = {};
+
+        err.inner.forEach(error => {
+          errorMessages[error.path] = error.message;
+        })
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      formRef.current.setData({
+        name: 'Marcio',
+        email: 'mjpancheri@gmail.com',
+        address:{
+          city: 'Batatais'
+        }
+      })
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello!</h1>
+      
+      <Form ref={formRef}  onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input type="email" name="email" />
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="number" />
+          <Input name="neighborhood" />
+          <Input name="city" />
+          <Input name="state" />
+        </Scope>
+        
+        <Input type="password" name="password" />
+
+        <button type="submit">Enviar</button>
+      </Form>
     </div>
   );
 }
